c1b3cc042a69e7f84eee5d83d36c75a3be9e6f54
Anton Larionov <larionov.nt@gmail.com>
2015-07-25 18:24:17 +0400

    [letras] Handle empty result page

--- prismriver/plugin/letras.py
+++ prismriver/plugin/letras.py
@@ -18,13 +18,16 @@ class LetrasPlugin(Plugin):
             soup = self.prepare_soup(page)
 
             header = soup.find('div', {'id': 'cabecalho'})
-            song_title = header.h1.a.text
-            song_artist = header.h2.a.text
 
-            lyric_block = soup.find('div', {'id': 'letra'})
+            # if header wasn't found - it's an empty page
+            if header:
+                song_title = header.h1.a.text
+                song_artist = header.h2.a.text
 
-            lyric = ''
-            for elem in lyric_block.findAll('p', recursive=False):
-                lyric += (self.parse_verse_block(elem) + '\n\n')
+                lyric_block = soup.find('div', {'id': 'letra'})
 
-            return Song(song_artist, song_title, self.sanitize_lyrics([lyric]))
+                lyric = ''
+                for elem in lyric_block.findAll('p', recursive=False):
+                    lyric += (self.parse_verse_block(elem) + '\n\n')
+
+                return Song(song_artist, song_title, self.sanitize_lyrics([lyric]))
