f4cea04a45d2a4d2d4284824f79ada98cbafce5c
Anton Larionov <larionov.nt@gmail.com>
2015-08-23 11:28:34 +0400

    Make script launchers executable

--- prismriver-lunasa.py
+++ prismriver-lunasa.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 from prismriver.qt import gui
 
 if __name__ == '__main__':
--- prismriver-lyrica.py
+++ prismriver-lyrica.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 from prismriver import cli
 
 if __name__ == '__main__':
