c2d5aef44ec9598dbfdcd8dbd16453c6782bca7f
Anton Larionov <larionov.nt@gmail.com>
2015-08-23 11:34:45 +0400

    [qt] Create window icon

--- prismriver/qt/gui.py
+++ prismriver/qt/gui.py
@@ -1,5 +1,7 @@
 import sys
 
+from PyQt5.QtGui import QIcon
+
 from PyQt5.QtWidgets import QApplication
 
 from prismriver.qt.window import MainWindow
@@ -10,5 +12,8 @@ def run():
     util.init_logging(False, True, None)
 
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon('prismriver/pixmaps/prismriver-lunasa.png'))
+
     main = MainWindow()
+
     sys.exit(app.exec_())
