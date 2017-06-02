4df1e0db5a5fd9c991c010b443589b69e43df2fe
Anton Larionov <larionov.nt@gmail.com>
2017-06-02 22:47:05 +0400

    [evesta] Fix search page parser

--- prismriver/plugin/evesta.py
+++ prismriver/plugin/evesta.py
@@ -9,26 +9,34 @@ class EvestaPlugin(Plugin):
         super(EvestaPlugin, self).__init__('Evesta', config)
 
     def search_song(self, artist, title):
-        link = 'http://www.evesta.jp/lyric/search2.php?t={}&ct=2&a={}&ca=2'.format(
+        link = 'http://lyric.evesta.jp/search.php?kind=title&keyword={}&how=2&do={}'.format(
             self.prepare_url_parameter(title),
-            self.prepare_url_parameter(artist))
+            self.prepare_url_parameter('検索'))
 
-        page = self.download_webpage(link)
+        page = self.download_webpage_text(link)
         if page:
             soup = self.prepare_soup(page)
 
-            search_result_pane = soup.find('div', {'id': 'lyricList'})
+            search_result_pane = soup.find('div', {'id': 'searchresult'})
             if search_result_pane is None:
                 return None
 
-            first_result = search_result_pane.table.findAll('tr')[1]
-            result_parts = first_result.findAll('a')
+            song_id = None
+            song_artist = None
+            song_title = None
 
-            song_title = result_parts[0].text
-            song_artist = result_parts[1].text
-            song_link = result_parts[0]['href']
+            for item in search_result_pane.findAll('div', {'class': 'result'}):
+                song_id = item.find('a', {'class': 'title'})['href']
+                song_title = item.find('a', {'class': 'title'}).text
+                song_artist = item.find('p', {'class': 'artist'}).a.text
 
-            lyric_page = self.download_webpage(song_link)
+                if self.compare_strings(title, song_title) and self.compare_strings(artist, song_artist):
+                    break
+
+            if not song_id:
+                return
+
+            lyric_page = self.download_webpage('http://lyric.evesta.jp' + song_id)
             soup = self.prepare_soup(lyric_page)
 
             lyric_pane = soup.find('div', {'id': 'lyricbody'})
